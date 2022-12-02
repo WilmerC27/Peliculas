8205e4da6449707f5e5566060b7ea630ce64cb7f
Daniel-LV <117936388+Daniel-LV@users.noreply.github.com>
2022-12-01 21:12:06 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,60 @@
+import peliculas from './peliculas.json' assert {type: 'json'};
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id')-1;
+
+
+console.log('ID: ' +id)
+
+console.log('xddd')
+console.log(peliculas[id])
+
+
+var imgPelicula = document.getElementById('img_pelicula');
+var tituloMain = document.getElementById('tituloMain');
+var titulo = document.getElementById('titulo');
+var sinopsis = document.getElementById('sinopsis');
+var cast = document.getElementById('cast');
+
+
+
+
+
+
+
+
+if(imgPelicula){
+    imgPelicula.setAttribute('src', 'assets/img/'+peliculas[id].image)
+}
+
+if(tituloMain){
+    tituloMain.innerHTML = peliculas[id].titulo;
+}
+if(titulo){
+    titulo.innerHTML = peliculas[id].titulo;
+}
+
+if(sinopsis){
+    sinopsis.innerHTML = peliculas[id].sinopsis;
+}
+
+if(cast){
+    console.log(peliculas[id].staff.cast)
+    peliculas[id].staff.cast.forEach(element => {
+        cast.innerHTML+=
+        `
+        <div class="col-5 pb-3" >
+        <div class="actor row">
+            <div class="imagen col-2">
+              <img class="" src="resources/img/actor.svg" alt="actor">
+            </div>
+            <div class="texto-descripcion col-6">
+              <p class="nombre">${element}</p>
+              <p class="papel">Lorem ipsum dolor sit amet Consectetu</p>
+            </div>
+        </div>
+      </div>
+        `
+    });
+}
---
+++ scrpitsglobal.js
@@ -0,0 +1,12 @@
+
+console.log('SI JALA')
+
+var navmain=document.getElementById("botonmain")
+if(navmain){
+    console.log("x")
+    if(localStorage.getItem("email") && localStorage.getItem("pass")){
+        console.log('d')
+        navmain.innerHTML='mis listas'
+    }
+}
+
